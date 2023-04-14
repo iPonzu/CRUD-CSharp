26422893a576d3276c3530fc318fe0e0a30b4d5c
Gabriel <101840148+gaturama@users.noreply.github.com>
2023-04-14 09:09:12 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
+using System;
 
 namespace App 
 {
     public class Program{
+        
         public static void Main(string[] args){
 
-            Console.WriteLine("Verdureira");
+            Console.WriteLine("Bem Vindo a FastFruits Verdureira");
             int op = 0;
             do{
                 Console.WriteLine("===========================");
@@ -24,6 +25,36 @@ namespace App
 
                 switch (op){
                 
+                    case 1: 
+                        View.Fabricante.CadastrarFabricante();
+                        break;
+                    case 2:
+                        View.Fabricante.AlterarFabricante();
+                        break;
+                    case 3:
+                        View.Fabricante.ExcluirFabricante();
+                        break;
+                    case 4:
+                        View.Fabricante.ListarFabricantes();
+                        break;
+                    case 5:
+                        View.Produto.CadastrarProduto();
+                        break;
+                    case 6: 
+                        View.Produto.AlterarProduto();
+                        break;
+                    case 7:
+                        View.Produto.ExcluirProduto();
+                        break;
+                    case 8:
+                        View.Produto.ListarProdutos();
+                        break;
+                    case 0:
+                        Console.WriteLine("Vazando...");
+                        break;
+                    default:
+                        Console.WriteLine("Opção Inválida");
+                        break;
                 }
 
             }while(op != 0);
